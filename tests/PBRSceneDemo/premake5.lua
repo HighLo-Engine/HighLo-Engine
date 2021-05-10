a5a478923dfc85bb03e01e2085cc341f20867d7e
FlareCoding <albert.slepak@gmail.com>
2021-05-09 22:17:06 -0400

    added a PBR Scene Demo project

---
+++ tests/PBRSceneDemo/premake5.lua
@@ -0,0 +1,65 @@
+project "PBRSceneDemo"
+    kind "ConsoleApp"
+    language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	entrypoint "mainCRTStartup"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+    files
+    { 
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+		"src",
+		"%{wks.location}/HighLo/src",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.glm}"
+    }
+
+    links
+    {
+        "HighLo"
+    }
+
+	defines
+	{
+		"HL_USE_DLL"
+	}
+
+	postbuildcommands
+	{
+		("{COPY} %{wks.location}HighLo/bin/" .. outputdir .. "/HighLo/HighLo.dll %{wks.location}tests/PBRSceneDemo/bin/" .. outputdir .. "/PBRSceneDemo/HighLo.dll*"),
+		("{COPY} %{wks.location}HighLo/vendor/assimp/lib/Debug/assimp-vc142-mtd.dll %{wks.location}tests/PBRSceneDemo/bin/" .. outputdir .. "/PBRSceneDemo/assimp-vc142-mtd.dll*"),
+		("{COPY} %{wks.location}HighLo/assets %{wks.location}tests/PBRSceneDemo/bin/" .. outputdir .. "/PBRSceneDemo/assets")
+	}
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        staticruntime "off"
+
+        disablewarnings { "5033" }
+
+        defines
+        {
+            "HL_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "HL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HL_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HL_DIST"
+        optimize "On"
---
+++ tests/PBRSceneDemo/src/PBRSceneDemo.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <HighLo.h>
+using namespace highlo;
+
+class PBRSceneDemo : public HLApplication
+{
+public:
+	void OnInitialize() override;
+	void OnUpdate(Timestep timestep) override;
+	void OnShutdown() override;
+	void OnEvent(Event &e) override;
+	void OnResize(uint32 width, uint32 height) override;
+
+private:
+	Light m_Light;
+	Ref<FPSCamera> m_Camera;
+	Ref<Environment> m_Environment;
+	Ref<Skybox> m_Skybox;
+
+	Ref<Mesh> m_PBR_Sphere;
+	Ref<Mesh> m_PBR_Gun;
+	Ref<Mesh> m_Pistol;
+	Ref<Mesh> m_Cowboy;
+
+	std::vector<Ref<Mesh>> m_TestSpheres;
+
+	void CreatePBRObjects();
+};
+

6c56beae5d5ef85be7f37df17b080e98874cc91f
Cankar001 <info.can-karka@web.de>
2021-05-29 00:12:51 +0200

    fixed FileMenuTest

--- tests/FileMenuTest/src/FileMenuTest.cpp
+++ tests/FileMenuTest/src/FileMenuTest.cpp
@@ -5,15 +5,15 @@ void FileMenuTest::OnInitialize()
 	GetWindow().CenterWindow();
 
 	Ref<FileMenu> fileMenu = FileMenu::Create("File");
-	fileMenu->AddMenuItem("New Scene", "", 1, [=]() { HL_WARN("Clicked New Scene"); });
-	fileMenu->AddMenuItem("Open Scene", "", 2, [=]() { HL_WARN("Clicked Open Scene"); });
+	fileMenu->AddMenuItem("New Scene", "", 1, [=](FileMenu *menu, MenuItem *item) { HL_WARN("Clicked New Scene"); });
+	fileMenu->AddMenuItem("Open Scene", "", 2, [=](FileMenu *menu, MenuItem *item) { HL_WARN("Clicked Open Scene"); });
 
 	fileMenu->AddSeparator();
 
 	Ref<FileMenu> zoomMenu = FileMenu::Create("Zoom");
-	zoomMenu->AddMenuItem("200%", "", 3, [=]() { HL_WARN("Clicked 200%"); });
-	zoomMenu->AddMenuItem("100%", "", 4, [=]() { HL_WARN("Clicked 100%"); });
-	zoomMenu->AddMenuItem("50%", "", 5, [=]() { HL_WARN("Clicked 50%"); });
+	zoomMenu->AddMenuItem("200%", "", 3, [=](FileMenu *menu, MenuItem *item) { HL_WARN("Clicked 200%"); });
+	zoomMenu->AddMenuItem("100%", "", 4, [=](FileMenu *menu, MenuItem *item) { HL_WARN("Clicked 100%"); });
+	zoomMenu->AddMenuItem("50%", "", 5, [=](FileMenu *menu, MenuItem *item) { HL_WARN("Clicked 50%"); });
 
 	fileMenu->AddSubMenu(zoomMenu);
 

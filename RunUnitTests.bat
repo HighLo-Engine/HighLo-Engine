953a26c05045c5f6a8951f5e1d0af4a19f8e7b08
Cankar001 <info.can-karka@web.de>
2021-08-05 17:05:18 +0200

    fixed build

--- RunUnitTests.bat
+++ RunUnitTests.bat
@@ -1,5 +1,14 @@
 @echo off
 
+echo preparing Unit Tests...
+
+echo copying dlls...
+ROBOCOPY HighLo\bin\Release-windows-x86_64\HighLo\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ HighLo.dll /mt /z
+ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ libcrypto-3-x64.dll /mt /z
+ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ libssl-3-x64.dll /mt /z
+ROBOCOPY HighLo\vendor\assimp\lib\Release\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ assimp-vc142-mt.dll /mt /z
+echo done preperation.
+
 echo Running String Unit Tests...
 call tests\StringTest\bin\Release-windows-x86_64\StringTest\StringTest.exe
 

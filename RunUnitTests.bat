801097ec370301fb82351de07cc47a4f5bbb9fa9
Cankar001 <info.can-karka@web.de>
2021-08-05 18:32:50 +0200

    fixed typo

--- RunUnitTests.bat
+++ RunUnitTests.bat
@@ -7,7 +7,7 @@ ROBOCOPY HighLo\bin\Release-windows-x86_64\HighLo\ tests\StringTest\bin\Release-
 ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ libcrypto-3-x64.dll /mt /z
 ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ libssl-3-x64.dll /mt /z
 ROBOCOPY HighLo\vendor\assimp\lib\Release\ tests\StringTest\bin\Release-windows-x86_64\StringTest\ assimp-vc142-mt.dll /mt /z
-echo done preperation.
+echo done preparation.
 
 echo Running String Unit Tests...
 call tests\StringTest\bin\Release-windows-x86_64\StringTest\StringTest.exe || echo Some Unit tests have failed.

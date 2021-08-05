0f0980800ca7cba139869170c8699a8576be0927
Cankar001 <info.can-karka@web.de>
2021-08-05 18:37:47 +0200

    undo unit test testing

--- tests/StringTest/src/Main.cpp
+++ tests/StringTest/src/Main.cpp
@@ -3,7 +3,6 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello World! Here will be the Unit tests for the String class!\n");
-	printf("TESTING what happens if the unit test would fail, by returning 1 the hole process should fail and stop!\n");
-	return 1;
+	return 0;
 }
 

a2750e748bac9ea8cb117dda832a820cbca80e8e
Cankar001 <info.can-karka@web.de>
2021-08-05 17:32:16 +0200

    testing what happens if the unit test would fail

--- tests/StringTest/src/Main.cpp
+++ tests/StringTest/src/Main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello World! Here will be the Unit tests for the String class!\n");
-
-	return 0;
+	printf("TESTING what happens if the unit test would fail, by returning 1 the hole process should fail and stop!\n");
+	return 1;
 }
+

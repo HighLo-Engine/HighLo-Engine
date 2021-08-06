8c6dcb0afca924c8bc94f90e7bf77af9deb34f7a
Cankar001 <info.can-karka@web.de>
2021-08-06 12:12:03 +0200

    changed assimp dll for testing

--- RunUnitTests.bat
+++ RunUnitTests.bat
@@ -6,7 +6,7 @@ ECHO copying dlls...
 ROBOCOPY HighLo\bin\Release-windows-x86_64\HighLo\ tests\StringTest\bin\ HighLo.dll /mt /z
 ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\ libcrypto-3-x64.dll /mt /z
 ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\ libssl-3-x64.dll /mt /z
-ROBOCOPY HighLo\vendor\assimp\lib\Release\ tests\StringTest\bin\ assimp-vc142-mt.dll /mt /z
+ROBOCOPY HighLo\vendor\assimp\lib\Debug\ tests\StringTest\bin\ assimp-vc142-mtd.dll /mt /z
 ROBOCOPY HighLo\vendor\HighLo-Unit\lib\ tests\StringTest\bin\ HighLo-Unit.dll /mt /z
 ECHO done preparation.
 

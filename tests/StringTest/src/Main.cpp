d77fd5897faace7fbbe07a3d0472f2c161e377f6
Cankar001 <info.can-karka@web.de>
2021-08-06 12:29:38 +0200

    rolled back unit tests

--- tests/StringTest/src/Main.cpp
+++ tests/StringTest/src/Main.cpp
@@ -4,6 +4,9 @@ using namespace highlo;
 bool test_append_to_string()
 {
 	highloUnit::Timer timer("test_append_to_string");
+	highloUnit::Ref<highloUnit::Console> console = highloUnit::Console::Create();
+	highloUnit::ConsoleForeground foreground = highloUnit::ConsoleForeground::GREEN;
+	highloUnit::ConsoleBackground background = highloUnit::ConsoleBackground::BLACK;
 
 	HLString str = "Hello";
 	HLString str2 = " World!";
@@ -11,29 +14,68 @@ bool test_append_to_string()
 	HLString expected = "Hello World!";
 
 	highloUnit::UnitTest test;
-	return test.AssertEqual(timer, *expected, *result);
+	bool unitTestResult = test.AssertEqual(timer, *expected, *result);
+	if (unitTestResult)
+	{
+		console->WriteLine("Test test_append_to_string succeeded!", foreground, background);
+	}
+	else
+	{
+		foreground = highloUnit::ConsoleForeground::RED;
+		console->WriteLine("Test test_append_to_string failed!", foreground, background);
+	}
+
+	return unitTestResult;
 }
 
 bool test_remove_from_string()
 {
 	highloUnit::Timer timer("test_remove_from_string");
+	highloUnit::Ref<highloUnit::Console> console = highloUnit::Console::Create();
+	highloUnit::ConsoleForeground foreground = highloUnit::ConsoleForeground::GREEN;
+	highloUnit::ConsoleBackground background = highloUnit::ConsoleBackground::BLACK;
 	
 	HLString str = "Hello World!";
 	str -= " World!";
 	HLString expected = "Hello";
 
 	highloUnit::UnitTest test;
-	return test.AssertEqual(timer, *expected, *str);
+	bool unitTestResult = test.AssertEqual(timer, *expected, *str);
+	if (unitTestResult)
+	{
+		console->WriteLine("Test test_remove_from_string succeeded!", foreground, background);
+	}
+	else
+	{
+		foreground = highloUnit::ConsoleForeground::RED;
+		console->WriteLine("Test test_remove_from_string failed!", foreground, background);
+	}
+
+	return unitTestResult;
 }
 
 bool test_empty_string()
 {
 	highloUnit::Timer timer("test_empty_string");
-	
+	highloUnit::Ref<highloUnit::Console> console = highloUnit::Console::Create();
+	highloUnit::ConsoleForeground foreground = highloUnit::ConsoleForeground::GREEN;
+	highloUnit::ConsoleBackground background = highloUnit::ConsoleBackground::BLACK;
+
 	HLString emptyStr = "";
 
 	highloUnit::UnitTest test;
-	return test.AssertEqual<uint32>(timer, emptyStr.Length(), 2);
+	bool unitTestResult = test.AssertEqual<uint32>(timer, emptyStr.Length(), 0);
+	if (unitTestResult)
+	{
+		console->WriteLine("Test test_empty_string succeeded!", foreground, background);
+	}
+	else
+	{
+		foreground = highloUnit::ConsoleForeground::RED;
+		console->WriteLine("Test test_empty_string failed!", foreground, background);
+	}
+
+	return unitTestResult;
 }
 
 int main(int argc, char *argv[])
@@ -47,9 +89,6 @@ int main(int argc, char *argv[])
 
 	highloUnit::UnitTest unitTest;
 	unitTest.AppendAllTests(funcs);
-	
-	int exitCode = unitTest.ExecuteTests();
-	std::cout << "Exit Code: " << exitCode << std::endl;
-	return exitCode;
+	return unitTest.ExecuteTests();
 }
 

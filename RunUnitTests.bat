eb02882e345e1d16489600950d4dfa4109afbcc8
Cankar001 <info.can-karka@web.de>
2021-08-22 16:15:32 +0200

    fixed build

--- RunUnitTests.bat
+++ RunUnitTests.bat
@@ -8,6 +8,12 @@ ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\ libcrypto-3-x64.dll /m
 ROBOCOPY HighLo\vendor\openssl\lib\ tests\StringTest\bin\ libssl-3-x64.dll /mt /z > nul
 ROBOCOPY HighLo\vendor\assimp\lib\Debug\ tests\StringTest\bin\ assimp-vc142-mtd.dll /mt /z > nul
 ROBOCOPY HighLo\vendor\HighLo-Unit\lib\ tests\StringTest\bin\ HighLo-Unit.dll /mt /z > nul
+
+ROBOCOPY HighLo\bin\Release-windows-x86_64\HighLo\ tests\EncryptionTest\bin\ HighLo.dll /mt /z > nul
+ROBOCOPY HighLo\vendor\openssl\lib\ tests\EncryptionTest\bin\ libcrypto-3-x64.dll /mt /z > nul
+ROBOCOPY HighLo\vendor\openssl\lib\ tests\EncryptionTest\bin\ libssl-3-x64.dll /mt /z > nul
+ROBOCOPY HighLo\vendor\assimp\lib\Debug\ tests\EncryptionTest\bin\ assimp-vc142-mtd.dll /mt /z > nul
+ROBOCOPY HighLo\vendor\HighLo-Unit\lib\ tests\EncryptionTest\bin\ HighLo-Unit.dll /mt /z > nul
 ECHO OK: dlls have been copied!
 ECHO.
 

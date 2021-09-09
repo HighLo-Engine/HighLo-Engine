34fd4edcf817847b10b612e1a2d79dfd518efda8
Cankar001 <info.can-karka@web.de>
2021-09-09 04:02:45 +0200

    fixed unit test generation

--- tests/premake5.lua
+++ tests/premake5.lua
@@ -1,8 +1,29 @@
-include "StringTest"
-include "EncryptionTest"
-include "ListTest"
-include "HashmapTest"
-include "VectorTest"
-include "StackTest"
-include "QueueTest"
+include "../vendor/bin/premake/solution_items.lua"
+include "../Dependencies.lua"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "HighLo-Tests"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+    startproject "Sandbox"
+
+	solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+
+	include "StringTest"
+	include "EncryptionTest"
+	include "ListTest"
+	include "HashmapTest"
+	include "VectorTest"
+	include "StackTest"
+	include "QueueTest"
 

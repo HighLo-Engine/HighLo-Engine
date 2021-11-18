8a59d774bd5625199bb7b2127bbd6391e556bed7
Cankar001 <info.can-karka@web.de>
2021-11-18 21:38:36 +0100

    updated String tests

--- tests/src/Main.cpp
+++ tests/src/Main.cpp
@@ -1,13 +1,10 @@
+// Copyright (c) 2021 Can Karka and Albert Slepak. All rights reserved.
+
 #include <HighLo.h>
 #include <gtest/gtest.h>
 
 #include "tests/StringTests.h"
 #include "tests/EncryptionTests.h"
-
-TEST(DemoTest, DemoTest)
-{
-	EXPECT_EQ(4, 2 + 2);
-}
 
 int main(int argc, char *argv[])
 {

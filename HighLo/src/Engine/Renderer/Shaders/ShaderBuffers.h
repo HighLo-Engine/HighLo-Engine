42846e67aa6b8ccdedf0e962045b8621ecd8f191
Cankar001 <info.can-karka@web.de>
2021-12-21 14:07:26 +0100

    Added missing copyright headers

--- HighLo/src/Engine/Renderer/Shaders/ShaderBuffers.h
+++ HighLo/src/Engine/Renderer/Shaders/ShaderBuffers.h
@@ -1,5 +1,11 @@
-#pragma once
+// Copyright (c) 2021 Can Karka and Albert Slepak. All rights reserved.
 
+//
+// version history:
+//     - 1.0 (2021-12-21) initial release
+//
+
+#pragma once
 
 namespace highlo
 {

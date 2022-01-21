c4ec51f8e09bb34f8a05e4ad516858e192aa3b42
Cankar001 <info.can-karka@web.de>
2022-01-21 17:20:42 +0100

    fixed MeshLoading

--- HighLo/src/Engine/Loaders/MeshLoader.h
+++ HighLo/src/Engine/Loaders/MeshLoader.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2021 Can Karka and Albert Slepak. All rights reserved.
+
+//
+// version history:
+//     - 1.0 (2022-01-21) initial release
+//
+
 #pragma once
 
 #include "Engine/Core/FileSystemPath.h"
@@ -19,40 +26,40 @@ namespace highlo
 	{
 	public:
 
-		virtual ~MeshLoader() {}
+		HLAPI virtual ~MeshLoader() {}
 
-		virtual const Ref<VertexBuffer> &GetVertexBuffer() const = 0;
-		virtual const Ref<IndexBuffer> &GetIndexBuffer() const = 0;
-		virtual const BufferLayout &GetLayout() const = 0;
+		HLAPI virtual const Ref<VertexBuffer> &GetVertexBuffer() const = 0;
+		HLAPI virtual const Ref<IndexBuffer> &GetIndexBuffer() const = 0;
+		HLAPI virtual const BufferLayout &GetLayout() const = 0;
 
 		// Materials
-		virtual const std::vector<Ref<Material>> &GetMaterials() const = 0;
-		virtual const std::vector<Ref<Texture2D>> &GetTextures() const = 0;
-		virtual const std::vector<Ref<Texture2D>> &GetNormalMaps() const = 0;
+		HLAPI virtual const std::vector<Ref<Material>> &GetMaterials() const = 0;
+		HLAPI virtual const std::vector<Ref<Texture2D>> &GetTextures() const = 0;
+		HLAPI virtual const std::vector<Ref<Texture2D>> &GetNormalMaps() const = 0;
 
 		// Anim
-		virtual const glm::mat4 &GetInverseTransform() const = 0;
-		virtual const std::vector<glm::mat4> &GetBoneTransforms() const = 0;
-		virtual const std::unordered_map<uint32, std::vector<Triangle>> &GetTriangleCache() const = 0;
-		virtual const std::vector<Mesh> &GetSubmeshes() const = 0;
+		HLAPI virtual const glm::mat4 &GetInverseTransform() const = 0;
+		HLAPI virtual const std::vector<glm::mat4> &GetBoneTransforms() const = 0;
+		HLAPI virtual const std::unordered_map<uint32, std::vector<Triangle>> &GetTriangleCache() const = 0;
+		HLAPI virtual const std::vector<Mesh> &GetSubmeshes() const = 0;
 
-		virtual uint32 GetBoneCount() const = 0;
-		virtual const std::unordered_map<HLString, uint32> &GetBoneMappings() const = 0;
-		virtual const std::vector<BoneInfo> &GetBoneInfos() const = 0;
-		virtual bool IsAnimated() const = 0;
-		virtual const FileSystemPath &GetFilePath() const = 0;
-		virtual const AABB &GetBoundingBox() const = 0;
+		HLAPI virtual uint32 GetBoneCount() const = 0;
+		HLAPI virtual const std::unordered_map<HLString, uint32> &GetBoneMappings() const = 0;
+		HLAPI virtual const std::vector<BoneInfo> &GetBoneInfos() const = 0;
+		HLAPI virtual bool IsAnimated() const = 0;
+		HLAPI virtual const FileSystemPath &GetFilePath() const = 0;
+		HLAPI virtual const AABB &GetBoundingBox() const = 0;
 
-		virtual const std::vector<Vertex> &GetStaticVertices() const = 0;
-		virtual const std::vector<AnimatedVertex> &GetAnimatedVertices() const = 0;
-		virtual const std::vector<VertexIndex> &GetIndices() const = 0;
+		HLAPI virtual const std::vector<Vertex> &GetStaticVertices() const = 0;
+		HLAPI virtual const std::vector<AnimatedVertex> &GetAnimatedVertices() const = 0;
+		HLAPI virtual const std::vector<VertexIndex> &GetIndices() const = 0;
 
-		virtual float GetTicksPerSecond() const = 0;
-		virtual float GetAnimationDuration() const = 0;
+		HLAPI virtual float GetTicksPerSecond() const = 0;
+		HLAPI virtual float GetAnimationDuration() const = 0;
 
-		virtual void ManipulateBoneTransform(float time, std::vector<glm::mat4> &outTransforms) = 0;
+		HLAPI virtual void ManipulateBoneTransform(float time, std::vector<glm::mat4> &outTransforms) = 0;
 
-		static Ref<MeshLoader> Create(const FileSystemPath &filePath, const Ref<Shader> &shader);
+		HLAPI static Ref<MeshLoader> Create(const FileSystemPath &filePath, const Ref<Shader> &shader);
 	};
 }
 

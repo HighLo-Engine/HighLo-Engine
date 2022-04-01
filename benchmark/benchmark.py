f90e880f4e71da488ba907220fd5a4dd6f807f0c
Can Karka <53074877+Cankar001@users.noreply.github.com>
2022-04-01 16:55:56 +0300

    Added option to choose between manual or automatic binary file input

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -11,7 +11,28 @@ else:
 
 levelEditor = "HighLoEdit.exe"
 
-def findPathOfFolder(filename):
+def YesOrNo(prefix):
+    while True:
+        try:
+            reply = str(input(prefix + ' [Y/N]: ')).lower().strip()
+            if reply[:1] == 'y':
+                return True
+            if reply[:1] == 'n':
+                return False
+        except KeyboardInterrupt:
+            return False
+
+def getUserInput():
+    try:
+        reply = str(input('>>'))
+        if len(reply) == 0:
+            return None
+        
+        return reply
+    except KeyboardInterrupt:
+        return None
+
+def findPath(filename):
     result = []
     
     # get_drives is now platform independent, on windows system it returns C:, D:, etc. and under unix systems it returns /
@@ -25,12 +46,51 @@ def findPathOfFolder(filename):
     return result
         
 
-if __name__ == "__main__":
-    print("Searching for " + levelEditor + " on your pc...")
-    paths = findPathOfFolder(levelEditor)
+def pathsAreValid(paths):
+    found = False
+
+    for path in paths:
+        if levelEditor in path:
+            found = True
+
+    # TODO: If we add support for more binaries in the future (e.g. Sandbox.exe or Runtime.exe)
+    # we also have to validate them here
+
+    return found
+
+def getBinaryPaths():
+    paths = []
+    autoDetect = YesOrNo("Would you like to automatically detect the binary files of the engine?")
+    if autoDetect:
+        print("Searching for " + levelEditor + " on your pc...")
+        paths = findPath(levelEditor)
+    else:
     
-    print("found these paths:")
+        while True:
+            print("Please provide a path to the file you want to benchmark:")
+            path = getUserInput()
+            if path:
+                paths.append(path)
+                print("You entered: " + path)
+                
+            doBreak = YesOrNo("Did you enter all paths you want to benchmark?")
+            if doBreak:
+                break
+            
+    # NEVER TRUST THE USER: because the user did enter .exe paths that are about to be executed we first have to check if the files are really part of the engine
+    if pathsAreValid(paths):
+        # now we have guaranteed input paths of the .exe files of the engine
+        return paths
+    else:
+        print("You did not enter paths that are part of the engine. Stop.")
+        quit()
+
+if __name__ == "__main__":
+    paths = getBinaryPaths()    
+    print("these are your paths:")
+            
     for path in paths:
         print(path)
-    
+            
+            
     

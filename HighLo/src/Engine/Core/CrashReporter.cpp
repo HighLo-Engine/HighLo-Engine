af195cc5fed8424cfff53b5caf97894e74ce4b60
Cankar001 <info.can-karka@web.de>
2022-04-22 12:47:41 +0200

    Added CrashReporter for asserts

---
+++ HighLo/src/Engine/Core/CrashReporter.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2021-2022 Can Karka and Albert Slepak. All rights reserved.
+
+#include "HighLoPch.h"
+#include "CrashReporter.h"
+
+#include "Engine/Graphics/Shaders/ShaderCache.h"
+#include "Engine/Core/Timer.h"
+
+namespace highlo
+{
+	void CrashReporter::Crash()
+	{
+		HL_CORE_FATAL("The Engine is about to crash! going to write recovery files and all caches into their files!");
+		ShaderCache::Shutdown();
+
+		Timer::Sleep(10);
+	}
+}
+

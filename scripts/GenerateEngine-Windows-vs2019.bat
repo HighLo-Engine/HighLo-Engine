400baeb95d05d14de3a698eddae8c7fe81f1e316
Can Karka <53074877+Cankar001@users.noreply.github.com>
2022-07-01 12:57:25 +0200

    cleaned up build script

--- scripts/GenerateEngine-Windows-vs2019.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-PUSHD ..
-CALL vendor\bin\premake\Windows\premake5.exe vs2019
-POPD
-
--- scripts/GenerateEngine-Windows-vs2022.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-PUSHD ..
-CALL vendor\bin\premake\Windows\premake5.exe vs2022
-POPD
-

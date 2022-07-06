7d895c18019643687d541ebaff77bcec0cd3ec7c
Can Karka <53074877+Cankar001@users.noreply.github.com>
2022-07-06 14:29:06 +0200

    optimized shader layouts and added uniform layouts

--- HighLo/src/Engine/Graphics/UniformLayout.cpp
+++ HighLo/src/Engine/Graphics/UniformLayout.cpp
@@ -3,6 +3,94 @@
 
 namespace highlo
 {
+	UniformLayout UniformLayout::GetCameraLayout()
+	{
+		return {
+			{ "u_Camera.ViewProjectionMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.InverseViewProjectionMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.ProjectionMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.InverseProjectionMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.ViewMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.InverseViewMatrix", UniformLayoutDataType::Mat4 },
+			{ "u_Camera.NDCToViewMul", UniformLayoutDataType::Float2 },
+			{ "u_Camera.NDCToViewAdd", UniformLayoutDataType::Float2 },
+			{ "u_Camera.DepthUnpackConsts", UniformLayoutDataType::Float2 },
+			{ "u_Camera.CameraTanHalfFOV", UniformLayoutDataType::Float2 },
+		};
+	}
+
+	UniformLayout UniformLayout::GetScreenDataLayout()
+	{
+		return {
+			{ "u_ScreenData.InvFullResolution", UniformLayoutDataType::Float2 },
+			{ "u_ScreenData.FullResolution", UniformLayoutDataType::Float2 },
+			{ "u_ScreenData.InvHalfResolution", UniformLayoutDataType::Float2 },
+			{ "u_ScreenData.HalfResolution", UniformLayoutDataType::Float2 },
+		};
+	}
+
+	UniformLayout UniformLayout::GetRendererDataLayout()
+	{
+		return {
+			{ "u_RendererData.CascadeSplits", UniformLayoutDataType::Float4 },
+			{ "u_RendererData.LightSize", UniformLayoutDataType::Float },
+			{ "u_RendererData.MaxShadowDistance", UniformLayoutDataType::Float },
+			{ "u_RendererData.ShadowFade", UniformLayoutDataType::Float },
+			{ "u_RendererData.CascadeTransitionFade", UniformLayoutDataType::Float },
+			{ "u_RendererData.TilesCountX", UniformLayoutDataType::Int },
+			{ "u_RendererData.ShowLightComplexity", UniformLayoutDataType::Bool },
+			{ "u_RendererData.ShowCascades", UniformLayoutDataType::Bool },
+			{ "u_RendererData.SoftShadows", UniformLayoutDataType::Bool },
+			{ "u_RendererData.CascadeFading", UniformLayoutDataType::Bool },
+		};
+	}
+
+	UniformLayout UniformLayout::GetHBAODataLayout()
+	{
+		return {
+			{ "u_HBAO.Float2Offsets", UniformLayoutDataType::Float4, 16 },
+			{ "u_HBAO.Jitters", UniformLayoutDataType::Float4, 16 },
+			{ "u_HBAO.PerspectiveInfo", UniformLayoutDataType::Float4 },
+			{ "u_HBAO.RadiusToScreen", UniformLayoutDataType::Float },
+			{ "u_HBAO.NegInvR2", UniformLayoutDataType::Float },
+			{ "u_HBAO.NDotVBias", UniformLayoutDataType::Float },
+			{ "u_HBAO.AOMultiplier", UniformLayoutDataType::Float },
+			{ "u_HBAO.PowExponent", UniformLayoutDataType::Float },
+			{ "u_HBAO.Padding1", UniformLayoutDataType::Float3 },
+			{ "u_HBAO.ShadowTolerance", UniformLayoutDataType::Float },
+			{ "u_HBAO.InvQuarterResolution", UniformLayoutDataType::Float2 },
+			{ "u_HBAO.Padding2", UniformLayoutDataType::Float2 },
+			{ "u_HBAO.IsOrtho", UniformLayoutDataType::Bool },
+		};
+	}
+
+	UniformLayout UniformLayout::GetShadowDataLayout()
+	{
+		return {
+			{ "u_DirShadow.DirLightMatrices", UniformLayoutDataType::Mat4, 4 },
+		};
+	}
+
+	UniformLayout UniformLayout::GetSceneDataLayout()
+	{
+		return {
+			// this is a custom struct, so insert only bytesize (or in this case the struct is exactly 2 * vec4 bytes)
+			{ "u_Scene.DirectionalLights", UniformLayoutDataType::Float4, 2 },
+
+			{ "u_Scene.CameraPosition", UniformLayoutDataType::Float3 },
+			{ "u_Scene.EnvironmentMapIntensity", UniformLayoutDataType::Float },
+		};
+	}
+
+	UniformLayout UniformLayout::GetPointLightDataLayout()
+	{
+		return {
+			// TODO:											make this uint
+			{ "u_PointLights.LightCount", UniformLayoutDataType::Int },
+			{ "u_PointLights.Lights", UniformLayoutDataType::Float4, 3 }, // again custom struct, but this time it is 3 * vec4
+		};
+	}
+
 	void UniformLayout::CalculateOffsetsAndStride()
 	{
 		uint32 offset = 0;
--- HighLo/src/Engine/Graphics/UniformLayout.h
+++ HighLo/src/Engine/Graphics/UniformLayout.h
@@ -11,8 +11,63 @@ namespace highlo
 	{
 		HLString Name;
 		UniformLayoutDataType Type = UniformLayoutDataType::Float;
+		uint32 TypeCount = 1;
 		uint32 Size = 0;
 		uint32 Offset = 0;
+
+		HLAPI UniformLayoutElement(const HLString &name, UniformLayoutDataType type, uint32 typeCount = 1)
+			: Name(name), Type(type), TypeCount(typeCount)
+		{
+			Size = GetComponentSize();
+		}
+
+		HLAPI UniformLayoutElement(const HLString &name, UniformLayoutDataType type, uint32 typeCount = 1, uint32 size = 0, uint32 offset = 0)
+			: Name(name), Type(type), TypeCount(typeCount), Size(size), Offset(offset)
+		{
+		}
+
+		HLAPI uint32 GetComponentCount() const
+		{
+			switch (Type)
+			{
+				case UniformLayoutDataType::Float:   return 1;
+				case UniformLayoutDataType::Float2:  return 2;
+				case UniformLayoutDataType::Float3:  return 3;
+				case UniformLayoutDataType::Float4:  return 4;
+				case UniformLayoutDataType::Mat3:    return 3 * 3;
+				case UniformLayoutDataType::Mat4:    return 4 * 4;
+				case UniformLayoutDataType::Int:     return 1;
+				case UniformLayoutDataType::Int2:    return 2;
+				case UniformLayoutDataType::Int3:    return 3;
+				case UniformLayoutDataType::Int4:    return 4;
+				case UniformLayoutDataType::Bool:    return 1;
+			}
+
+			return 0;
+		}
+
+		HLAPI uint32 GetComponentSize() const
+		{
+			if (Size > 0)
+				return Size;
+
+			switch (Type)
+			{
+				case UniformLayoutDataType::Float:    return TypeCount * 4;
+				case UniformLayoutDataType::Float2:   return TypeCount * 4 * 2;
+				case UniformLayoutDataType::Float3:   return TypeCount * 4 * 3;
+				case UniformLayoutDataType::Float4:   return TypeCount * 4 * 4;
+				case UniformLayoutDataType::Mat3:     return TypeCount * 4 * 3 * 3;
+				case UniformLayoutDataType::Mat4:     return TypeCount * 4 * 4 * 4;
+				case UniformLayoutDataType::Int:      return TypeCount * 4;
+				case UniformLayoutDataType::Int2:     return TypeCount * 4 * 2;
+				case UniformLayoutDataType::Int3:     return TypeCount * 4 * 3;
+				case UniformLayoutDataType::Int4:     return TypeCount * 4 * 4;
+				case UniformLayoutDataType::Bool:     return TypeCount * 1;
+			}
+
+			return 0;
+		}
 	};
 
 	class UniformLayout
@@ -28,6 +83,14 @@ namespace highlo
 		{
 			CalculateOffsetsAndStride();
 		}
+
+		HLAPI static UniformLayout GetCameraLayout();
+		HLAPI static UniformLayout GetScreenDataLayout();
+		HLAPI static UniformLayout GetRendererDataLayout();
+		HLAPI static UniformLayout GetHBAODataLayout();
+		HLAPI static UniformLayout GetShadowDataLayout();
+		HLAPI static UniformLayout GetSceneDataLayout();
+		HLAPI static UniformLayout GetPointLightDataLayout();
 
 		HLAPI uint32 GetStride() const { return m_Stride; }
 

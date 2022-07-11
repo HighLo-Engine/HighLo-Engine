fbcd3bec4a890a4892bbd244924bdd5f43b7bec9
Cankar001 <Info.Can-Karka@Web.de>
2022-07-11 21:42:09 +0200

    Refactored ToUTF8, ToUTF16 and ToUTF32 to be part of the string class

--- HighLo/src/Engine/Utils/StringUtils.h
+++ HighLo/src/Engine/Utils/StringUtils.h
@@ -11,8 +11,6 @@
 
 #pragma once
 
-#include <codecvt>
-
 namespace highlo
 {
 	namespace utils
@@ -26,34 +24,6 @@ namespace highlo
 			}
 
 			return false;
-		}
-
-		static HLString32 ToUTF32(const HLString &str)
-		{
-			std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
-			std::u32string result = conv.from_bytes(*str);
-			return HLString32(result.c_str(), (uint32)result.length());
-		}
-
-		static HLString16 ToUTF16(const HLString &str)
-		{
-			std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> conv;
-			std::u16string result = conv.from_bytes(*str);
-			return HLString16(result.c_str(), (uint32)result.length());
-		}
-
-		static HLString ToUTF8(const HLString32 &str)
-		{
-			std::wstring_convert<std::codecvt_utf8<int32>, int32> conv;
-			const int32 *p = reinterpret_cast<const int32*>(*str);
-			return conv.to_bytes(p, p + str.Length());
-		}
-
-		static HLString ToUTF8(const HLString16 &str)
-		{
-			std::wstring_convert<std::codecvt_utf8<int16>, int16> conv;
-			const int16 *p = reinterpret_cast<const int16*>(*str);
-			return conv.to_bytes(p, p + str.Length());
 		}
 
 		static HLString FillWithLeading(HLString &str, const HLString &leadingCharacter, uint32 count = 1)

55bcc2693f6f678eebca5929bc55a8328cbcd816
Cankar001 <Info.Can-Karka@Web.de>
2022-07-11 21:42:49 +0200

    improved 2D shaders

--- HighLo/assets/shaders/2D/Renderer2DLine.glsl
+++ HighLo/assets/shaders/2D/Renderer2DLine.glsl
@@ -7,10 +7,6 @@ layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in int a_EntityID;
 
-layout(location = 3) in vec4 a_MRow0;
-layout(location = 4) in vec4 a_MRow1;
-layout(location = 5) in vec4 a_MRow2;
-
 struct VertexOutput
 {
 	vec4 Color;
@@ -21,18 +17,8 @@ layout(location = 5) out flat int v_EntityID;
 
 void main()
 {
-	/*
-	mat4 transform = mat4(
-		vec4(a_MRow0.x, a_MRow1.x, a_MRow2.x, 0.0),
-		vec4(a_MRow0.y, a_MRow1.y, a_MRow2.y, 0.0),
-		vec4(a_MRow0.z, a_MRow1.z, a_MRow2.z, 0.0),
-		vec4(a_MRow0.w, a_MRow1.w, a_MRow2.w, 1.0)
-	);
-	*/
-
 	Output.Color = a_Color;
 	v_EntityID = a_EntityID;
-	//gl_Position = u_Camera.ViewProjectionMatrix * transform * vec4(a_Position, 1.0f);
 	gl_Position = u_Camera.ViewProjectionMatrix * vec4(a_Position, 1.0f);
 }
 
--- HighLo/assets/shaders/2D/Renderer2DText.glsl
+++ HighLo/assets/shaders/2D/Renderer2DText.glsl
@@ -8,10 +8,6 @@ layout(location = 1) in vec2 a_TexCoord;
 layout(location = 2) in vec4 a_Color;
 layout(location = 3) in float a_TexIndex;
 
-layout(location = 4) in vec4 a_MRow0;
-layout(location = 5) in vec4 a_MRow1;
-layout(location = 6) in vec4 a_MRow2;
-
 struct VertexOutput
 {
 	vec4 Color;
@@ -23,19 +19,9 @@ layout(location = 5) out flat float v_TexIndex;
 
 void main()
 {
-	/*
-	mat4 transform = mat4(
-		vec4(a_MRow0.x, a_MRow1.x, a_MRow2.x, 0.0),
-		vec4(a_MRow0.y, a_MRow1.y, a_MRow2.y, 0.0),
-		vec4(a_MRow0.z, a_MRow1.z, a_MRow2.z, 0.0),
-		vec4(a_MRow0.w, a_MRow1.w, a_MRow2.w, 1.0)
-	);
-	*/
-
 	Output.Color = a_Color;
 	Output.TexCoord = a_TexCoord;
 	v_TexIndex = a_TexIndex;
-	//gl_Position = u_Camera.ViewProjectionMatrix * transform * vec4(a_Position, 1.0);
 	gl_Position = u_Camera.ViewProjectionMatrix * vec4(a_Position, 1.0);
 }
 
@@ -49,6 +35,8 @@ struct VertexOutput
 };
 
 layout(location = 0) out vec4 o_Color;
+
+
 layout(location = 0) in VertexOutput Input;
 layout(location = 5) in flat float v_TexIndex;
 layout(binding = 1) uniform sampler2D u_FontAtlases[32];

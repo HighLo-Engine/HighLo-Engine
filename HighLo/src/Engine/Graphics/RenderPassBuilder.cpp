036a5e8993c8137ed8a7a338b955dd221ed5acaf
Cankar001 <Info.Can-Karka@Web.de>
2022-07-28 18:23:58 +0200

    Added basic renderpass builder

---
+++ HighLo/src/Engine/Graphics/RenderPassBuilder.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021-2022 Can Karka and Albert Slepak. All rights reserved.
+
+#include "HighLoPch.h"
+#include "RenderPassBuilder.h"
+
+namespace highlo
+{
+	RenderPassBuilder::RenderPassBuilder()
+	{
+	}
+
+	RenderPassBuilder::~RenderPassBuilder()
+	{
+	}
+
+
+}
+

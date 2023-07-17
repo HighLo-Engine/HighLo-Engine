759c9badac5ebde345f1a1319efee27df3a607d6
Cankar001 <Info.Can-Karka@Web.de>
2023-07-17 22:17:00 +0200

    Revert "fixed some errors in the linux build"
    This reverts commit ab0dce35a7208a80bfa7f26173b8af36febac805.

--- HighLo/src/Engine/Core/Allocator.h
+++ HighLo/src/Engine/Core/Allocator.h
@@ -35,14 +35,14 @@ namespace highlo
 		HLAPI const Byte &operator[](uint32 i) const;
 
 		template<typename T>
-		HLAPI HL_FORCE_INLINE T &Read(uint32 offset = 0)
+		HLAPI FORCEINLINE T &Read(uint32 offset = 0)
 		{
 			HL_ASSERT(offset < Size);
 			return *(T*)(Data + offset);
 		}
 
 		template<typename T>
-		HLAPI HL_FORCE_INLINE T *As()
+		HLAPI FORCEINLINE T *As()
 		{
 			return (T*)Data;
 		}
